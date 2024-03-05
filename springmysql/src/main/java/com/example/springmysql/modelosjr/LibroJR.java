6cf34267dc21da5f82023b751df26739c10b6473
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-05 09:50:13 -0500

    Update LibroJR.java

--- springmysql/src/main/java/com/example/springmysql/modelosjr/LibroJR.java
+++ springmysql/src/main/java/com/example/springmysql/modelosjr/LibroJR.java
@@ -1,4 +1,24 @@
 package com.example.springmysql.modelosjr;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "librosjr")
+@Data
 public class LibroJR {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_librojr;
+    private String isbnjr;
+    private int numerospagjr;
+    @CreationTimestamp
+    private LocalDate fechapublicacionjr;
+    private String titulo;
+
+    //relacionar con autores
+
 }
