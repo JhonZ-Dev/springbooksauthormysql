15f7c406819794ce0d48de59a6599f55366094f4
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-05 10:12:24 -0500

    Update AutoresRepoJR.java

--- springmysql/src/main/java/com/example/springmysql/repositoriosjr/AutoresRepoJR.java
+++ springmysql/src/main/java/com/example/springmysql/repositoriosjr/AutoresRepoJR.java
@@ -1,4 +1,9 @@
 package com.example.springmysql.repositoriosjr;
 
-public interface AutoresRepoJR {
+import com.example.springmysql.modelosjr.AutoresJR;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutoresRepoJR extends JpaRepository<AutoresJR, Long> {
 }
