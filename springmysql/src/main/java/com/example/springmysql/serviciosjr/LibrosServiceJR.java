d1a9945a5ebf2292172f9e7027fe8247010cb77d
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-05 17:28:59 -0500

    Update LibrosServiceJR.java

--- springmysql/src/main/java/com/example/springmysql/serviciosjr/LibrosServiceJR.java
+++ springmysql/src/main/java/com/example/springmysql/serviciosjr/LibrosServiceJR.java
@@ -1,4 +1,21 @@
 package com.example.springmysql.serviciosjr;
 
+import com.example.springmysql.modelosjr.AutoresJR;
+import com.example.springmysql.modelosjr.LibroJR;
+import com.example.springmysql.repositoriosjr.LibrosRepoJr;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LibrosServiceJR {
+
+    @Autowired
+    private LibrosRepoJr librosRepoJr;
+
+    //metodo para guardar
+    public LibroJR guardarlibros(LibroJR libroJR, AutoresJR id_autor){
+        //relacion
+        libroJR.setId_autor(id_autor);
+        return librosRepoJr.save(libroJR);
+    }
 }
